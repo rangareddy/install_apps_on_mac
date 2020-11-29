a181a97d6d28e201729e7fcbd41fd08c4ef90b64
Ranga Reddy <rangareddy.avula@gmail.com>
2020-11-29 20:40:43 +0530

    Added gradle installation

--- Install_Apps_on_Mac.sh
+++ Install_Apps_on_Mac.sh
@@ -17,9 +17,8 @@ HOMEBREW_URL='https://raw.githubusercontent.com/Homebrew/install/master/install'
 JAVA_HOME_PATH="/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home"
 
 command_exists () {
-    type "$1" &> /dev/null ;
+  type "$1" &> /dev/null ;
 }
-
 
 if ! command_exists brew ; then
   echo "Installing HomeBrew..." 
@@ -75,6 +74,26 @@ if ! command_exists mvn ; then
   echo "Installing Maven"
   brew install 'maven'
 
+  MAVEN_HOME=`mvn --version | grep 'Maven home:' | cut -d ':' -f 2  | cut -d ' ' -f 2`
+  source ~/.bash_profile;
+
+  if [ -z "$M2_HOME" ]; then
+      echo "export M2_HOME=$MAVEN_HOME" >> ~/.bash_profile;
+      echo "export PATH=\$M2_HOME/bin:\$PATH" >> ~/.bash_profile;
+  fi
+  cp $MAVEN_HOME/conf/settings.xml ~/.m2/
+fi
+
+if ! command_exists gradle ; then
+  echo "Installing Gradle"
+  brew install gradle
+  GRADLE_VERSION=`gradle --version | grep 'Gradle ' | cut -d ' ' -f 2`
+  GRADLE_HOME_DIR='/usr/local/Cellar/gradle/$GRADLE_VERSION/'
+
+  if [ -z "$GRADLE_HOME" ]; then
+      echo "export GRADLE_HOME=$GRADLE_HOME_DIR" >> ~/.bash_profile;
+      echo "export PATH=\$GRADLE_HOME/bin:\$PATH" >> ~/.bash_profile;
+  fi
 fi
 
 if ! command_exists idea ; then
