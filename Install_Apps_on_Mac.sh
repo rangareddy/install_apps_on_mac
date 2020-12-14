0245fe8604f16e309644a4af1f6b87166dc126df
Ranga Reddy <rangareddy.avula@gmail.com>
2020-12-14 09:51:23 +0530

    Updating Instllation notes"

--- Install_Apps_on_Mac.sh
+++ Install_Apps_on_Mac.sh
@@ -20,6 +20,7 @@ command_exists () {
   type "$1" &> /dev/null ;
 }
 
+# brew
 if ! command_exists brew ; then
   echo "Installing HomeBrew..." 
   # supress the need to press 'return' when in the install script runs. 
@@ -31,26 +32,33 @@ if ! command_exists brew ; then
   exit 0
 fi
 
+brew update
+
+# telnet
 if ! command_exists telnet ; then
   echo "Installing telnet..."
   brew install telnet
 fi
 
+# git version tool
 if ! command_exists git ; then
   echo "Installing git..."
   brew install git
 fi
 
+# wget
 if ! command_exists wget ; then
   echo "Installing wget..."
   brew install wget
 fi
 
+# visual studio
 if ! command_exists code ; then
   echo "Installing Visual Studio code..."
   brew cask install --appdir='/Applications' 'visual-studio-code'
 fi
 
+# java8
 checkAndInstallJava() {
   source ~/.bash_profile;
   if [ -z "$JAVA_HOME" ]; then
@@ -70,8 +78,28 @@ checkAndInstallJava() {
 
 checkAndInstallJava
 
+# scala
+if ! command_exists scala ; then
+  echo "Installing Scala"
+  brew install 'scala@2.12'
+
+  source ~/.bash_profile;
+  if [ -z "$SCALA_HOME" ]; then
+    
+    SCALA_HOME_DIR=(/usr/local/Cellar/scala*/*)
+    echo "Exporting the SCALA_HOME=$SCALA_HOME_DIR to .bash_profile"
+    echo "export SCALA_HOME=$SCALA_HOME_DIR" >> ~/.bash_profile
+    echo "export PATH=\$SCALA_HOME/bin:\$PATH" >> ~/.bash_profile
+    source ~/.bash_profile
+    echo "Verifying SCALA_HOME path"
+    echo $SCALA_HOME
+  fi
+
+fi
+
+# maven
 if ! command_exists mvn ; then
-  echo "Installing Maven"
+  echo "Installing Maven build tool"
   brew install 'maven'
 
   MAVEN_HOME=`mvn --version | grep 'Maven home:' | cut -d ':' -f 2  | cut -d ' ' -f 2`
@@ -84,8 +112,9 @@ if ! command_exists mvn ; then
   cp $MAVEN_HOME/conf/settings.xml ~/.m2/
 fi
 
+## gradle
 if ! command_exists gradle ; then
-  echo "Installing Gradle"
+  echo "Installing Gradle build tool"
   brew install gradle
   GRADLE_VERSION=`gradle --version | grep 'Gradle ' | cut -d ' ' -f 2`
   GRADLE_HOME_DIR='/usr/local/Cellar/gradle/$GRADLE_VERSION/'
@@ -96,17 +125,26 @@ if ! command_exists gradle ; then
   fi
 fi
 
+# sbt
+if ! command_exists sbt ; then
+  echo "Installing sbt build tool"
+  brew install sbt
+fi
+
+# intellij idea
 if ! command_exists idea ; then
   echo "Installing IntelliJ Community Edition..."
   brew cask install --appdir='/Applications' 'intellij-idea-ce'
   sudo ln -s "/Applications/IntelliJ\ IDEA\ CE.app/Contents/MacOS/idea" /usr/local/bin/idea
 fi
 
+# sublime text editor
 if ! command_exists subl ; then
   echo "Installing Sublime text..."
   brew cask install  --appdir='/Applications' 'sublime-text'
 fi
 
+# mysql --> username root and password root
 if ! command_exists mysql ; then
   echo "Installing MySQL..."
   brew install mysql@5.7
